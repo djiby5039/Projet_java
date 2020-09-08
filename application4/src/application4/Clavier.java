b2f50986b3797a149d8fbe2c10cbe43e4c258e54
Djiby_Sow <djiby5039@gmail.com>
2020-09-08 17:01:48 +0000

    Add files via upload

---
+++ application4/src/application4/Clavier.java
@@ -0,0 +1,81 @@
+package application4;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Ghislain Akinocho
+ * @since 29.10.2019 12:00
+ * @
+ */
+public class Clavier {
+	
+	// L'unique attribut de l'objet
+	private static Scanner scanner = null;
+	
+	public  Clavier() {}
+	
+	public static void init () {
+		scanner = new Scanner (System.in);
+	}
+	/**
+	 * Cette fonction lit une chaine de caractère au clavier
+	 * @return
+	 */
+	public static String lireString () {
+		init (); 
+		String chaineLue = scanner.nextLine();
+		return chaineLue;
+	}
+	/**
+	 * Cette fonction lit un int au clavier
+	 * @return
+	 */
+	public static int lireInt () {
+		String chaineLue = lireString();
+		return Integer.parseInt(chaineLue);
+	}
+	/**
+	 * Cette fonction lit un float au clavier
+	 * @return
+	 */
+	public static float lireFloat () {
+		String chaineLue = lireString();
+		return Float.parseFloat(chaineLue);
+	}
+	/**
+	 * Cette fonction lit un double au clavier
+	 * @return
+	 */
+	public static double lireDouble () {
+		String chaineLue = lireString();
+		return Double.parseDouble(chaineLue);
+	}
+	/**
+	 * Cette fonction lit un boolean au clavier
+	 * @return
+	 **/
+	public static boolean lireBoolean () {
+		String chaineLue = lireString();
+		return Boolean.parseBoolean(chaineLue);
+	}
+	/**
+	 * Cette fonction lit un caractère au clavier
+	 * @return
+	 */
+	public static char lireChar () {
+		String chaineLue = lireString();
+		if(chaineLue.length() > 0){
+		
+		return chaineLue.charAt(0);
+	}
+		else{
+			return 'x';}
+		}
+	
+	public static void close ( ) {
+		if (scanner != null) {
+			scanner.close () ;
+		}
+	}
+}
---
+++ application4/src/application4/TestRationnel.java
@@ -0,0 +1,15 @@
+package application4;
+
+public class TestRationnel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+        	
+        }
+        
+
+	}
+
+
